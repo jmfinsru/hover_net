62155206acc26379a7f49f6f7b8022f3fbbf6c78
jenny <jenny.finsrud@gmail.com>
2025-02-18 15:33:44 +0100

    test sh file

---
+++ 2_run_tile.sh
@@ -0,0 +1,16 @@
+python run_infer.py \
+--gpu='0,1' \
+--nr_types=6 \
+--type_info_path=type_info.json \
+--batch_size=64 \
+--model_mode=fast \
+--model_path=/media/jenny/PRIVATE_USB/Hover_net_files/Pannuke_checkpoints/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--nr_inference_workers=1 \
+--nr_post_proc_workers=1 \
+tile \
+--input_dir=/media/jenny/PRIVATE_USB/MM_Andregangsstudiet_HE_20x_JPEG/HE_MM009_2_270125.vsi.Collection/HE_MM009_2_270125_20x_BF_01 \
+--output_dir=/media/jenny/PRIVATE_USB/Hover_net_files/Output/Tile \
+--mem_usage=0.8 \
+--draw_dot \
+--save_qupath
+
