12acfd46c43232139fa011e2dcf4ae0ca966f51b
jenny <jenny.finsrud@gmail.com>
2025-02-18 15:40:13 +0100

    gpu=1 to gpu=0,1

--- run_tile.sh
+++ run_tile.sh
@@ -1,15 +1,16 @@
 python run_infer.py \
---gpu='1' \
+--gpu='0,1' \
 --nr_types=6 \
 --type_info_path=type_info.json \
 --batch_size=64 \
 --model_mode=fast \
---model_path=../pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
---nr_inference_workers=8 \
---nr_post_proc_workers=16 \
+--model_path=/media/jenny/PRIVATE_USB/Hover_net_files/Pannuke_checkpoints/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--nr_inference_workers=1 \
+--nr_post_proc_workers=1 \
 tile \
---input_dir=dataset/sample_tiles/imgs/ \
---output_dir=dataset/sample_tiles/pred/ \
+--input_dir=/media/jenny/PRIVATE_USB/MM_Andregangsstudiet_HE_20x_JPEG/HE_MM009_2_270125.vsi.Collection/HE_MM009_2_270125_20x_BF_01/HE_MM009_2_270125_20x_BF_01.jpg \
+--output_dir=/media/jenny/PRIVATE_USB/Hover_net_files/Output/Tile \
 --mem_usage=0.1 \
 --draw_dot \
 --save_qupath
+
