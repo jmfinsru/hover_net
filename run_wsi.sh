23b8b7bd846ecd481d56b0c5e0d2994687b36d1c
jenny <jenny.finsrud@gmail.com>
2025-03-04 09:41:24 +0100

    added mask path

--- run_wsi.sh
+++ run_wsi.sh
@@ -2,14 +2,14 @@ python run_infer.py \
 --gpu='0,1' \
 --nr_types=6 \
 --type_info_path=type_info.json \
---batch_size=64 \
+--batch_size=16 \
 --model_mode=fast \
---model_path=../pretrained/hovernet_fast_pannuke_type_tf2pytorch.tar \
---nr_inference_workers=8 \
---nr_post_proc_workers=16 \
+--model_path=/media/jenny/PRIVATE_USB/Hover_net_files/Pannuke_checkpoints/hovernet_fast_pannuke_type_tf2pytorch.tar \
+--nr_inference_workers=2 \
+--nr_post_proc_workers=4 \
 wsi \
---input_dir=dataset/sample_wsis/wsi/ \
---output_dir=dataset/sample_wsis/out/ \
---input_mask_dir=dataset/sample_wsis/msk/ \
+--input_dir=/media/jenny/PRIVATE_USB/Converted_images \
+--output_dir=/media/jenny/PRIVATE_USB/Hover_net_files/Output/WSI \
+--input_mask_dir=/media/jenny/PRIVATE_USB/masks_test/ \
 --save_thumb \
 --save_mask
